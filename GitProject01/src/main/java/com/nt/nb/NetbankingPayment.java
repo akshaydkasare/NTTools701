06ffe71a81f27eadf08459eea9403309d33ee295
akshaydkasare <akshaydkasare@gmail.com>
2025-06-05 16:28:53 +0530

    TL commiting code

--- GitProject01/src/main/java/com/nt/nb/NetbankingPayment.java
+++ GitProject01/src/main/java/com/nt/nb/NetbankingPayment.java
@@ -2,6 +2,7 @@ package com.nt.nb;
 
 public class NetbankingPayment {
 
+	//NetBanking Payment
 		public String nbpayment(String srcAccNo, String destAccNo) {
 			return "Payment Successfully";
 		}
